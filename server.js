1b01e5f189a68046dc4ea681887c853477d1353a
SK Sabir Ali <sksabirali89@gmail.com>
2023-07-10 23:35:52 +0530

    setup express mongo

---
+++ server.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+const corsOptions = {
+  origin: "http://localhost:8081",
+};
+app.use(cors(corsOptions));
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// Secret key for JWT
+const secretKey = "your-secret-key";
+
+// Example user object (replace with your own user database or API)
+const users = [
+  { id: 1, username: "admin", password: "admin123", role: "admin" },
+  { id: 2, username: "user", password: "user123", role: "user" },
+];
+
+// Store refresh tokens (replace with your own storage solution)
+const refreshTokens = [];
+
+// Generate JWT token
+function generateToken(user, expiresIn) {
+  return jwt.sign({ id: user.id, role: user.role }, secretKey, { expiresIn });
+}
+
+// Generate refresh token
+function generateRefreshToken() {
+  const refreshToken = jwt.sign({}, secretKey, { expiresIn: "7d" });
+  refreshTokens.push(refreshToken);
+  return refreshToken;
+}
+
+// Middleware to verify JWT token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+}
+
+// Login endpoint
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => u.username === username);
+  console.log("user", user);
+
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    return res.status(401).json({ message: "Invalid username or password" });
+  }
+
+  const token = generateToken(user, "15m");
+  const refreshToken = generateRefreshToken();
+  res.json({ token, refreshToken });
+});
+
+// Refresh token endpoint
+app.post("/api/refresh-token", (req, res) => {
+  const { refreshToken } = req.body;
+
+  if (!refreshToken || !refreshTokens.includes(refreshToken)) {
+    return res.sendStatus(403);
+  }
+
+  jwt.verify(refreshToken, secretKey, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    const token = generateToken(user, "15m");
+    res.json({ token });
+  });
+});
+
+// Protected endpoint example
+app.get("/api/protected", authenticateToken, (req, res) => {
+  res.json({
+    message: "Protected endpoint accessed successfully",
+    user: req.user,
+  });
+});
+
+require("./app/routes/turorial.routes")(app);
+
+app.listen(8080, () => {
+  console.log("Server is running on http://localhost:8080");
+});
